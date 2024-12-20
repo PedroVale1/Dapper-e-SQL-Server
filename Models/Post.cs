ea8336972bb16cd4f71479361b1f22fb6a3f7016
PedroVale1 <162756423+PedroVale1@users.noreply.github.com>
2024-12-19 23:53:40 -0300

    Add files via upload

---
+++ Models/Post.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using Dapper.Contrib.Extensions;
+
+namespace Blog.Models
+{
+    [Table("[Post]")]
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
